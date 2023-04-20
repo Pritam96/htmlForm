5e9462acaf1aad95134a406e3020775262aa8a0f
Pritam96 <pritammondal96@gmail.com>
2023-04-20 20:56:27 +0530

    Task 12 - Lets Scale the app to more users

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var form = document.getElementById('my-form');
 var msg = document.querySelector('.msg');
+var items = document.querySelector('.items');
 
-const person = {};
 var count = 0;
 
 form.addEventListener('submit', savetoLocalStorage);
@@ -9,25 +9,34 @@ form.addEventListener('submit', savetoLocalStorage);
 function savetoLocalStorage(e) {
   e.preventDefault();
 
-  var name = document.getElementById('name');
-  var email = document.getElementById('email');
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
 
-  person.name = name.value;
-  person.email = email.value;
+  let person = { name, email };
 
   var keyName = 'person' + count;
 
-  if (name.value === '' || email.value === '') {
+  if (name === '' || email === '') {
     msg.classList.add('error');
     msg.innerHTML = 'Please enter all fields';
     setTimeout(() => msg.remove(), 3000);
   } else {
+    localStorage.setItem(keyName, JSON.stringify(person));
+    addToList();
+    form.reset();
+    count++;
     msg.classList.add('success');
     msg.innerHTML = 'Successfully saved your information';
     setTimeout(() => msg.remove(), 3000);
-    localStorage.setItem(keyName, JSON.stringify(person));
-    count++;
-    name.value = '';
-    email.value = '';
   }
+}
+
+function addToList() {
+  var per = JSON.parse(localStorage.getItem('person' + count));
+
+  // creating list item
+  var li = document.createElement('li');
+  li.className = 'item';
+  li.appendChild(document.createTextNode(per.name + ' - ' + per.email));
+  items.appendChild(li);
 }
